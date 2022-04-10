9beec3da6e41db4a62615947aa27792dd6cc3a88
Or Hasson <hassonor@gmail.com>
2022-04-10 16:52:59 +0300

    setting eslint + prettier

--- website-with-node-and-express/server.js
+++ website-with-node-and-express/server.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import path from 'path';
 
-
 const app = express();
 
-const PORT = 3003 | process.env.PORT;
+const PORT = 3003 || process.env.PORT;
 
-app.use(express.static(path.join(__dirname, './static')))
-
+app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/index.html'));
+  response.sendFile(path.join(__dirname, './static/index.html'));
 });
 
 app.get('/speakers', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'));
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
