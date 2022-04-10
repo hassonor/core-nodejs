136f5d38ab30aff8fcddda7ea02b04613d520ced
Or Hasson <hassonor@gmail.com>
2022-04-10 11:54:16 +0300

    setup static data and index and speakers route

--- website-with-node-and-express/server.js
+++ website-with-node-and-express/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import path from 'path';
+
 
 const app = express();
 
 const PORT = 3003 | process.env.PORT;
 
+app.use(express.static(path.join(__dirname, './static')))
+
 
 app.get('/', (request, response) => {
-    response.send('Hello Or Hasson');
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
