d58607c42ac9a62defa7b072229c22ca6f52022d
Or Hasson <hassonor@gmail.com>
2022-11-19 13:18:14 +0200

    explain the depth-0 meaning

--- core-nodejs-getting-started/web-servers/native-http-demo/index.js
+++ core-nodejs-getting-started/web-servers/native-http-demo/index.js
@@ -1,15 +1,15 @@
-const http = require('http')
+const http = require("http");
 
 const requestListener = (req, res) => {
-    // req, res are streams!!
-    console.dir(req, {depth: 0})
-    res.write('Hello World\n')
-    res.end();
+  // req, res are streams!!
+  console.dir(req, { depth: 0 }); // depth = 0 means show only the first level of properties and any nested objects will not be printed here.
+  res.write("Hello World\n");
+  res.end();
 };
 
 const server = http.createServer();
-server.on('request', requestListener)
+server.on("request", requestListener);
 
 server.listen(4321, () => {
-    console.log('Server is running.... on port 4321')
-})
+  console.log("Server is running.... on port 4321");
+});
