db45ce7b54b4c460fa41532964276d7430d8901f
Or Hasson <hassonor@gmail.com>
2022-11-19 15:11:03 +0200

    remove package lock

--- core-nodejs-web-application-and-express/project1/app.js
+++ core-nodejs-web-application-and-express/project1/app.js
@@ -1,37 +1,38 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const sessions = require('./src/data/sessions.json');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const sessions = require("./src/data/sessions.json");
+
 const app = express();
 const sessionsRouter = express.Router();
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-sessionsRouter.route('/')
-    .get((req, res) => {
-        res.render('sessions', {
-            sessions
-        });
-    })
+sessionsRouter.route("/")
+  .get((req, res) => {
+    res.render("sessions", {
+      sessions
+    });
+  });
 
-sessionsRouter.route('/:id')
-    .get((req, res) => {
-        const id = req.params.id;
-        res.render('session', {session: sessions[id]});
-    })
+sessionsRouter.route("/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    res.render("session", { session: sessions[id] });
+  });
 
-app.use('/sessions', sessionsRouter);
+app.use("/sessions", sessionsRouter);
 
-app.get('/', (req, res) => {
-    res.render('index', {title: 'Or Hasson Site!', data: ['a', 'b', 'c']})
-})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Or Hasson Site!", data: ["a", "b", "c"] });
+});
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3003;
 
 app.listen(3003, () => {
-    console.log(`Listening on ${PORT}...`)
+  console.log(`Listening on ${PORT}...`);
 });
