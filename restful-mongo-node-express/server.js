5371122c04eb47249034e766fce8f62b3e6dc471
Or Hasson <hassonor@gmail.com>
2022-11-19 18:11:09 +0200

    remove body-parser libary

--- restful-mongo-node-express/server.js
+++ restful-mongo-node-express/server.js
@@ -5,8 +5,7 @@ if (process.env.NODE_ENV === "test") {
     ? "./config-prod.json"
     : "./config-dev.json");
 }
-const express = require("express");
-const bodyParser = require("body-parser");
+import  express from "express";
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
@@ -27,7 +26,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Prevent XSS attack:
 app.use(sanitize);
