6481bc6ac2af01f8adfd43a5cbca7d3832361478
Or Hasson <hassonor@gmail.com>
2022-11-19 13:09:14 +0200

    add explain to event emitter example

--- core-nodejs-getting-started/modules-and-concurrency/event-emitters/example.js
+++ core-nodejs-getting-started/modules-and-concurrency/event-emitters/example.js
@@ -1,4 +1,4 @@
-const EventEmitter = require('events');
+const EventEmitter = require("events");
 
 // Streams are Event Emitters
 // process.stdin, process.stdout
@@ -7,20 +7,24 @@ const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
 
 
+/*
+    This will be invoked after the rest of this programs is executed.
+    I put it in setImmediate instead moving it to last line of the file.
+ */
 setImmediate(() => {
-    myEmitter.emit('TEST_EVENT')
+  myEmitter.emit("TEST_EVENT");
 });
 
-myEmitter.on('TEST_EVENT', () => {
-    console.log("TEST_EVENT was fired")
+myEmitter.on("TEST_EVENT", () => {
+  console.log("TEST_EVENT was fired");
 });
 
-myEmitter.on('TEST_EVENT', () => {
-    console.log("TEST_EVENT was fired")
+myEmitter.on("TEST_EVENT", () => {
+  console.log("TEST_EVENT was fired");
 });
 
-myEmitter.on('TEST_EVENT', () => {
-    console.log("TEST_EVENT was fired")
+myEmitter.on("TEST_EVENT", () => {
+  console.log("TEST_EVENT was fired");
 });
 
 
