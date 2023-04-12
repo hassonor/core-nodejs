8ee8a6a327fd13db31a4d6260217619deaa05aed
Or Hasson <hassonor@gmail.com>
2023-04-12 20:54:16 +0300

    improve createServer example

--- nodejs-the-big-picture/httpModule/index.js
+++ nodejs-the-big-picture/httpModule/index.js
@@ -1,15 +1,16 @@
-// HTTP MODULE
 const http = require("http");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return;
   console.log(req.url);
-  const myURL = new URL(req.url, "http://localhost:5000");
+  let myURL = new URL(req.url, "http://localhost:5000");
   console.log(myURL);
 
-  res.writeHead(200, { "Context-Type": "text/html" });
-  res.end("<h1>Hey Or Hasson</h1>");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Welcome to My Node.js Server</h1>");
 });
 
-server.listen(5000);
+server.listen(5000, () => {
+  console.log("Server listening on port 5000");
+});
