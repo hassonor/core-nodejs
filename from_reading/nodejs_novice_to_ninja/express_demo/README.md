0ebe2dcc8d0491fd20dd5d59b7a33bb9843eb9bd
Or Hasson <hassonor@gmail.com>
2023-09-27 14:21:21 +0300

    expres + ejs + compression example

---
+++ from_reading/nodejs_novice_to_ninja/express_demo/README.md
@@ -0,0 +1,7 @@
+### Express Web Application Example
+
+1. `npm install`
+   Run the application with:
+
+2.`npm start`
+Then open paths at `http://localhost:3000/` in your web browser.
---
+++ from_reading/nodejs_novice_to_ninja/express_demo/index.js
@@ -0,0 +1,62 @@
+// Import essential modules
+import express from 'express'; // Framework for web applications
+import compression from 'compression'; // Middleware for compressing HTTP responses
+
+// Import utilities for file and directory paths in ES modules
+import {fileURLToPath} from 'url'; // Convert a module URL to a file path
+import {dirname, sep} from 'path'; // Extract directory name and get platform-specific path separator
+
+// Import router for the "/hello" route
+import {helloRouter} from "./routes/hello.js";
+
+// Determine the directory paths for various resources in the app
+const __dirname = dirname(fileURLToPath(import.meta.url)) + sep,
+    cfg = {
+        port: process.env.PORT || 3000, // Set the server port
+        dir: {
+            root: __dirname,
+            static: __dirname + 'static' + sep,
+            views: __dirname + 'views' + sep,
+            routes: __dirname + 'routes' + sep
+        }
+    };
+
+// Display configuration details for debugging and verification
+console.dir(cfg, {depth: null, color: true});
+
+// Create an Express application instance
+const app = express();
+
+// Improve security by removing the header that identifies the server technology
+app.disable('x-powered-by');
+
+// Set up the view engine for server-side templating
+app.set('view engine', 'ejs');
+app.set('views', cfg.dir.views);
+
+// Apply response compression to reduce data transfer sizes
+app.use(compression());
+
+// Serve static files (like images, stylesheets, and scripts) from the specified directory
+app.use(express.static(cfg.dir.static));
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.render('message', {title: 'Hello Or Hasson! How are you today?'});
+})
+
+// Use the imported router for "/hello" routes
+app.use('/hello', helloRouter);
+
+// Handle 404 errors for unhandled routes
+app.use((req, res) => {
+    res.status(404).render('message', {title: 'Not found'});
+});
+
+// Start the Express server on the designated port
+app.listen(cfg.port, () => {
+    console.log(`Server listening at http://localhost:${cfg.port}`);
+})
+
+// Make the configuration and app instance available for imports in other modules
+export {cfg, app};
