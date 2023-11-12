ca48a86da4b112e95dcb0c7251a429519113ca26
Or Hasson <hassonor@gmail.com>
2023-11-12 09:56:43 +0200

    remove old staf

--- security-best-practices/lib/logout.js
+++
@@ -1,8 +0,0 @@
-function logout(req, res) {
-    // Destroy the session
-    res.clearCookie('loggedInUser');
-    // Redirect back to the home page
-    res.redirect('/index.html');
-}
-
-module.exports = logout;
